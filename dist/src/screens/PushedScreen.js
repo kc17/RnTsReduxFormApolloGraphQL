e50a0edc095824dd11efd2fa03fb2e7e9e5587fb
線上讀書會 <polo13999@gmail.com>
2018-03-08 07:20:52 +0800

    update

--- dist/src/screens/PushedScreen.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
-class PushedScreen extends React.Component {
-    render() {
-        return (<View>      <Text>        第2      </Text>    </View>);
-    }
-}
-export default PushedScreen;
-//# sourceMappingURL=PushedScreen.js.map
--- dist/src/screens/SecondTabScreen.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
-class SecondTabScreen extends React.Component {
-    render() {
-        return (<View>      <Text>        第2      </Text>    </View>);
-    }
-}
-export default SecondTabScreen;
-//# sourceMappingURL=SecondTabScreen.js.map
